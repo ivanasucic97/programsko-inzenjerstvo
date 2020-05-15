f9c2a515b64e0db272ddb29ca207c8891ace7337
ivanasucic97 <44435087+ivanasucic97@users.noreply.github.com>
2020-05-15 20:16:10 +0200

    Add files via upload

---
+++ mine.py
@@ -0,0 +1,82 @@
+Python 2.7.13 (v2.7.13:a06454b1afa1, Dec 17 2016, 20:42:59) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> class Kvadrat(object):
+
+    def __init__(self, broj = 0):
+        self.__broj = broj
+        self.__otkriven = False
+        self.__oznaka = False
+
+    def otkrij(self):
+        if self.__otkriven == False and self.__oznaka == False:
+            self.__otkriven = True
+
+    def oznaci(self):
+        if self.__oznaka == False:
+            self.__oznaka = True
+        else:
+            self.__oznaka = False
+
+    @property
+    def jeMina(self):
+        if self.__broj == -1:
+            return True
+        else:
+            return False
+
+    @property
+    def jeBroj(self):
+        if self.__broj != 0 and self.__broj != -1:
+            return True
+        else:
+            return False
+
+    @property
+    def jePrazan(self):
+        if self.__broj == 0:
+            return True
+        else:
+            return False
+
+    def __str__(self):
+##        tekst = ""
+##        if self.__otkriven:
+##            tekst += "otkriven "
+##        else:
+##            tekst += "!otkriven "
+##        if self.__oznaka:
+##            tekst += "oznacen"
+##        else:
+##            tekst += "!oznacen"
+##        print(tekst)
+        if self.__oznaka == True:
+            return "?"
+        if self.__otkriven == False:
+            return "."
+        else:
+            if self.jeMina == True:
+                return "x"
+            if self.jeBroj == True:
+                return "%d" % self.__broj
+            if self.jePrazan == True:
+                return " "
+
+
+print('*** test 1 ***')
+brojevi = [-1,0,1,2]
+for broj in brojevi:
+    k = Kvadrat(broj)
+    print(k.jeMina, k.jeBroj, k.jePrazan)
+
+print('*** test 2 ***')
+kvadrati = [Kvadrat(broj) for broj in [-1,0,1,2]]
+print('   oz ot oz ot')
+for k in kvadrati:
+    rez = []
+    rez.append(str(k))
+    for _ in range(2):
+        k.oznaci()
+        rez.append(str(k))
+        k.otkrij()
+        rez.append(str(k))
+    print('%s  %s  %s  %s  %s  ' % tuple(rez))
